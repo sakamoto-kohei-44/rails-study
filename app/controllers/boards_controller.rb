6899156179bf852f3203c20bc106bb716bcc5c98
sakamotokohei <your-email@banbengongping111@gmail.com>
2023-06-28 19:56:26 +0900

    国際化

---
+++ app/controllers/boards_controller.rb
@@ -0,0 +1,25 @@
+class BoardsController < ApplicationController
+  def index
+    @boards = Board.all.includes(:user).order(created_at: :desc)
+  end
+
+  def new
+    @board = Board.new
+  end
+
+  def create
+    @board = Board.new(board_params)
+    if @board.save
+      redirect_to boards_path, notice: t('flash.notices.board_created')
+    else
+      flash.now[:alert] = t('flash_message.bordcreation_filed')
+      render :new
+    end
+  end
+
+  private
+
+  def board_params
+    params.require(:board).permit(:title, :body)
+  end
+end
