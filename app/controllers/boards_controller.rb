14882baa4ea15a449dc910a4bc71cabf3367235b
sakamoto-kohei-44 <banbengongping111@gmail.com>
2023-07-12 07:18:47 +0900

    課題14

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,4 +1,5 @@
 class BoardsController < ApplicationController
+  before_action :set_board, only: %i[edit update destroy]
   def index
     @boards = Board.all.includes(:user).order(created_at: :desc)
   end
@@ -6,6 +7,8 @@ class BoardsController < ApplicationController
   def new
     @board = Board.new
   end
+
+  def edit; end
 
   def create
     @board = current_user.boards.build(board_params)
@@ -23,7 +26,25 @@ class BoardsController < ApplicationController
     @comments = @board.comments.includes(:user).order(created_at: :desc)
   end
 
+  def update
+    if @board.update(board_params)
+      redirect_to @board, success: t('defaults.message.updated', item: Board.model_name.human)
+    else
+      flash.now[:danger] = t('defaults.message.not_updated', item: Board.model_name.human)
+      render :edit
+    end
+  end
+
+  def destroy
+    @board.destroy!
+    redirect_to boards_path, success: t('defaults.message.deleted', item: Board.model_name.human)
+  end
+
   private
+
+  def set_board
+    @board = current_user.boards.find(params[:id])
+  end
 
   def board_params
     params.require(:board).permit(:title, :body, :board_image, :board_image_cache)
