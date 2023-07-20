8d1f7eb91f7326271655dcfcf1770e6935d99e52
sakamoto-kohei-44 <banbengongping111@gmail.com>
2023-07-20 20:13:51 +0900

    検索機能

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,24 +1,26 @@
 class ProfilesController < ApplicationController
   before_action :set_user,only: %i[edit update]
+  def show
+    @user = current_user
+  end
 
-    def edit
-    end
+  def edit;end
 
-    def update
-        if @user.update(user_params)
-            redirect_to profile_path,success: "ユーザーを更新しました"
-        else
-            flash.now[:danger] = "ユーザーを更新できませんでした"
-            render :edit
-        end
+  def update
+    @user = current_user
+    if @user.update(user_params)
+      redirect_to profile_path, notice: 'プロフィールを更新しました'
+    else
+      render :edit
     end
+  end
 
-    private
-    def set_user
-        @user = User.find(current_user.id)
-    end
+  private
+  def set_user
+      @user = User.find(current_user.id)
+  end
 
-    def user_params
-        params.require(:user).permit(:email,:first_name,:last_name,:avatar,:avatar_cash)
-    end
+  def user_params
+      params.require(:user).permit(:email,:first_name,:last_name,:avatar,:avatar_cashe)
+  end
 end
