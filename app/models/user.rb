c21e007b75dec8f07f7277795f80f603d4ac6461
sakamoto-kohei-44 <banbengongping111@gmail.com>
2023-07-23 21:49:41 +0900

    ビューの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,7 @@ class User < ApplicationRecord
   validates :email, presence: true
   validates :first_name, presence: true, length: { maximum: 255 }
   validates :last_name, presence: true, length: { maximum: 255 }
+  validates :reset_password_token, presence: true, uniqueness: true, allow_nil: true
 
   def own?(object)
     id == object.user_id
