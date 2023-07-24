ea46548104822c6317dadd4bc6a9ea14b6437824
sakamoto-kohei-44 <banbengongping111@gmail.com>
2023-07-24 16:46:58 +0900

    修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -33,32 +33,4 @@ class User < ApplicationRecord
   def full_name
     "#{last_name} #{first_name}"
   end
-
-  def deliver_reset_password_instructions!
-    mail = false
-    config = sorcery_config
-    return false if config.reset_password_time_between_emails.present? && send(config.reset_password_email_sent_at_attribute_name) && send(config.reset_password_email_sent_at_attribute_name) > config.reset_password_time_between_emails.seconds.ago.utc
-
-    self.class.sorcery_adapter.transaction do
-      generate_reset_password_token!
-      mail = send_reset_password_email! unless config.reset_password_mailer_disabled
-    end
-    mail
-  end
-
-
-  def load_from_reset_password_token(token, &block)
-    load_from_token(
-      token,
-      @sorcery_config.reset_password_token_attribute_name,
-      @sorcery_config.reset_password_token_expires_at_attribute_name,
-      &block
-    )
-  end
-
-  def change_password(new_password, raise_on_failure: false)
-    clear_reset_password_token
-    send(:"#{sorcery_config.password_attribute_name}=", new_password)
-    sorcery_adapter.save raise_on_failure: raise_on_failure
-  end
 end
