3f5ae67c260489c6fee5767ca84f8b012f4c62a7
sakamoto-kohei-44 <banbengongping111@gmail.com>
2023-07-26 16:27:05 +0900

    ビューファイル

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,2 +1,14 @@
 class Admin::BaseController < ApplicationController
+  bofore_action :check_admin
+  layout 'admin/layouts/application'    # layout宣言
+
+  private
+
+  def not_authenticated
+    redirect_to admin_login_path, warning: t('defaults.message.require_login')
+  end
+
+  def check_admin
+    redirect_to root_path, warning: t('defaults.message.not_authorized') unless current_user.admin?
+  end
 end
--- app/controllers/admin/dashboards_controller.rb
+++ app/controllers/admin/dashboards_controller.rb
@@ -1,4 +1,3 @@
 class Admin::DashboardsController < ApplicationController
-  def index
-  end
+  def index; end
 end
